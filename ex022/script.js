fca41605eeb415353274376238dd85186dbac41c
GD-Finn <arthurzanatta901@gmail.com>
2026-05-29 20:08:13 -0300

    Update script.js

--- ex022/script.js
+++ ex022/script.js
@@ -1,12 +1,8 @@
-function verificar(){
-    let numero = Number(document.getElementById("numero").value);
-    let mensagem = document.getElementById("mensagem");
     mensagem.innerHTML = ""
 
-    mensagem.innerHTML = `<strong>Tabuada de ${numero}</strong><br><br>`
-
-    for (let contador = 1; contador <= 10; contador++) {
-
-    mensagem.innerHTML += `${numero} x ${contador} = ${numero * contador} <br>`;
-    }
-}
+function verificar(){let numero = Number(document.getElementById("numero").value);
+let mensagem = document.getElementById("mensagem");
+mensagem.innerHTML = `<strong>Tabuada de ${numero}</strong><br><br>`
+for (let contador = 1; contador <= 10; contador++) {
+mensagem.innerHTML += `${numero} x ${contador} = ${numero * contador} <br>`;} 
+} 
